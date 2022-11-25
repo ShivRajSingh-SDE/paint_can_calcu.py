5c7cc94e6f43247bc1683611f8c231c5cc06481a
Shivraj Singh <103109048+Shivraj-TaechBuddy@users.noreply.github.com>
2022-11-25 09:50:08 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,15 @@
+import math
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+height=test_h
+width= test_w
+cover= coverage
+
+def paint_calc(height,width,cover):
+  area= height* width
+  can_need = math.ceil(area/cover)
+  print(f"you need {can_need}")
+
+
+paint_calc(test_h, test_w, cover)a
